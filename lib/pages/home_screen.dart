15b20037479a7a67a9b0fb9040d61e1327afc3bb
viktorvoltz <vctrchinyeaka@gmail.com>
2023-04-18 04:33:08 +0100

    home screen

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('stellar client'),
+      ),
+    );
+  }
+}
