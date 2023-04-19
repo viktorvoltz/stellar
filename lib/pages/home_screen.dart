b1228d585f1136d45f0e85772f528bfb805c0a2c
viktorvoltz <vctrchinyeaka@gmail.com>
2023-04-19 21:07:26 +0100

    mnemonic

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:stellar/util/stellar_client.dart';
 import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  StellarClient stellarClient = StellarClient();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('stellar client'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("_event"),
+          onPressed: () async {
+            String mnemonic = await stellarClient.mnemonic();
+            print(mnemonic);
+          },
+        ),
       ),
     );
   }
